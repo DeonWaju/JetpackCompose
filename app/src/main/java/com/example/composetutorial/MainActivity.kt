ffa89348f085288195ee958faa47f2fce4505f97
deonolarewaju <deonolarewaju@gmail.com>
2022-11-09 12:13:47 +0100

    worked on image card

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,24 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            DefaultPreview()
         }
     }
 
@@ -29,24 +39,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column(
-        modifier = Modifier
-            .fillMaxHeight(1f)
-            .fillMaxWidth(1f)
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .background(Color.Blue)
-            .border(BorderStroke(2.dp, color = Color.Black))
+fun Greeting(
+    painter: Painter,
+    title: String,
+    desc: String,
+    modifier: Modifier = Modifier
+) {
+
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
     ) {
-        Text(text = "Hello $name!", modifier = Modifier.offset(20.dp, 50.dp))
-        Spacer(modifier = Modifier.height(50.dp))
-        Text(text = "Hello $name!")
-        Text(text = "World $name!")
+        Box(
+            modifier = modifier.height(200.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = desc,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 300f
+                        )
+                    )
+            )
+
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 16.sp)
+                )
+            }
+        }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Greeting("Dami")
+    val painter = painterResource(id = R.drawable.bvb)
+    val title = "Borrusia"
+    val desc = "This is BVB"
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
+    ) {
+        Greeting(
+            painter = painter,
+            title = title,
+            desc = desc
+        )
+    }
 }
