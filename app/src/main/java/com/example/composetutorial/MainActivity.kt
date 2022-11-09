364f2c2a324da857c07d1aaba289b67deada0670
deonolarewaju <deonolarewaju@gmail.com>
2022-11-09 12:40:13 +0100

    worked on horizontal lists using lazyrow

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -42,7 +43,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoadingList() {
-    LazyColumn{
+    LazyRow{
+
         items(400) {
             DefaultPreview()
         }
